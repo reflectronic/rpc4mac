c0a725e2a421d144db4639e15cc196692d9b2552
reflectronic <john-tur@outlook.com>
2020-04-16 21:49:31 -0400

    Begin implementing presence

--- Rpc4Mac/DiscordRichPresenceHandler.cs
+++ Rpc4Mac/DiscordRichPresenceHandler.cs
@@ -1,35 +1,42 @@
-﻿using MonoDevelop.Components.Commands;
+﻿using System;
+using System.Threading;
+using DiscordRPC;
+using MonoDevelop.Components.Commands;
 using MonoDevelop.Ide;
 using MonoDevelop.Ide.Gui;
+using MonoDevelop.Projects;
 
 namespace Rpc4Mac
 {
     public class DiscordRichPresenceHandler : CommandHandler
     {
+        DiscordRpcClient client;
+
+        RichPresence presence;
+
+        Timer timer;
+
         protected override void Run()
         {
-            IdeApp.Workbench.ActiveDocumentChanged += (sender, e) => UpdateRichPresence(e.Document);
+            IdeApp.Workbench.ActiveDocumentChanged += (sender, e) => UpdateRichPresence(e.Document, e.Document?.Owner);
 
-            IdeApp.FocusIn += (sender, e) => UpdateRichPresence(IdeApp.Workbench.ActiveDocument);
-
-            IdeApp.Workspace.LastWorkspaceItemClosed += (sender, e) => ShowNoWorkspaceRichPresence();
-
-            ShowNoWorkspaceRichPresence();
-        }
-
-        void ShowNoWorkspaceRichPresence()
-        {
-
-        }
-
-        void UpdateRichPresence(Document currentDocument)
-        {
-            if (currentDocument is null)
+            IdeApp.FocusIn += delegate
             {
-                // Show generic workspace info
-            }
+                var doc = IdeApp.Workbench.ActiveDocument;
+                UpdateRichPresence(doc, doc?.Owner);
+            };
 
-            MessageService.ShowMessage("Rich Presence Update", $"Your current document is {currentDocument?.Name ?? "None"}");
+            IdeApp.Workspace.LastWorkspaceItemClosed += (sender, e) => UpdateRichPresence(null, null);
+
+            client = new DiscordRpcClient("700501887937544262");
+            client.Initialize();
+
+            timer = new Timer(_ => client.SetPresence(presence), null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+        }
+
+        void UpdateRichPresence(Document document, WorkspaceObject workspace)
+        {
+            MessageService.ShowMessage("Rich Presence Update", $"Your current document is {document?.Name ?? "None"}");
         }
     }
 }
