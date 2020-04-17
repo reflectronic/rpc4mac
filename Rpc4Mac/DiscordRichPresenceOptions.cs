858d00fe979e8dae694201d0a4652eecd43d2aba
reflectronic <john-tur@outlook.com>
2020-04-17 14:51:25 -0400

    Wire up options UI to settings

--- Rpc4Mac/DiscordRichPresenceOptions.cs
+++ Rpc4Mac/DiscordRichPresenceOptions.cs
@@ -1,45 +1,17 @@
 ﻿using System;
-using AppKit;
-using Gtk;
-using MonoDevelop.Components;
-using MonoDevelop.Ide;
-using MonoDevelop.Ide.Gui.Dialogs;
+using System.Runtime.CompilerServices;
+using MonoDevelop.Core;
 
 namespace Rpc4Mac
 {
-    public class DiscordRichPresenceOptions : OptionsPanel
+    public static class DiscordRichPresenceOptions
     {
-        CheckButton enableButton = new CheckButton("Enable rich presence") { Xalign = 0 };
-        CheckButton showSolution = new CheckButton("Show current file") { Xalign = 0 };
-        CheckButton showProject = new CheckButton("Show current project") { Xalign = 0 };
-        CheckButton showElapsed = new CheckButton("Show elapsed time") { Xalign = 0 };
-        Entry customId = new Entry("700501887937544262");
+        public static readonly ConfigurationProperty<string> ApplicationId = ConfigurationProperty.Create(CreateName(), "700501887937544262");
+        public static readonly ConfigurationProperty<bool> EnableRichPresence = ConfigurationProperty.Create(CreateName(), true);
+        public static readonly ConfigurationProperty<bool> ShowCurrentFile = ConfigurationProperty.Create(CreateName(), true);
+        public static readonly ConfigurationProperty<bool> ShowCurrentProject = ConfigurationProperty.Create(CreateName(), true);
+        public static readonly ConfigurationProperty<bool> ShowElapsed = ConfigurationProperty.Create(CreateName(), true);
 
-        public override void ApplyChanges()
-        {
-
-        }
-
-        public override Control CreatePanelWidget()
-        {
-            var vbox = new VBox { Spacing = 6 };
-
-            var sectionLabel = new Label("<b>General</b>") { Xalign = 0, UseMarkup = true };
-            vbox.PackStart(sectionLabel, false, false, 0);
-
-            vbox.PackStart(enableButton, false, false, 0);
-            vbox.PackStart(showSolution, false, false, 0);
-            vbox.PackStart(showProject, false, false, 0);
-            vbox.PackStart(showElapsed, false, false, 0);
-
-            var id = new HBox { BorderWidth = 10, Spacing = 6 };
-            var idLabel = new Label("Application ID:") { Xalign = 0 };
-            id.PackStart(idLabel, false, false, 0);
-            id.PackStart(customId, true, true, 0);
-
-            vbox.PackStart(id, false, false, 0);
-            vbox.ShowAll();
-            return vbox;
-        }
+        static string CreateName([CallerMemberName] string name = "") => "Rpc4Mac." + name;
     }
 }
---
+++ Rpc4Mac/DiscordRichPresenceOptionsPanel.cs
@@ -0,0 +1,47 @@
+﻿using Gtk;
+using MonoDevelop.Components;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace Rpc4Mac
+{
+    public class DiscordRichPresenceOptionsPanel : OptionsPanel
+    {
+        CheckButton enableButton = new CheckButton("Enable rich presence") { Xalign = 0, Active = DiscordRichPresenceOptions.EnableRichPresence.Value };
+        CheckButton showFile = new CheckButton("Show current file") { Xalign = 0, Active = DiscordRichPresenceOptions.ShowCurrentFile.Value };
+        CheckButton showProject = new CheckButton("Show current project") { Xalign = 0, Active = DiscordRichPresenceOptions.ShowCurrentProject.Value };
+        CheckButton showElapsed = new CheckButton("Show elapsed time") { Xalign = 0, Active = DiscordRichPresenceOptions.ShowElapsed.Value };
+        Entry appId = new Entry(DiscordRichPresenceOptions.ApplicationId.Value);
+
+        public override Control CreatePanelWidget()
+        {
+            var vbox = new VBox { Spacing = 6 };
+
+            var sectionLabel = new Label("<b>General</b>") { Xalign = 0, UseMarkup = true };
+            vbox.PackStart(sectionLabel, false, false, 0);
+
+            vbox.PackStart(enableButton, false, false, 0);
+            vbox.PackStart(showFile, false, false, 0);
+            vbox.PackStart(showProject, false, false, 0);
+            vbox.PackStart(showElapsed, false, false, 0);
+
+            var id = new HBox { BorderWidth = 10, Spacing = 6 };
+            var idLabel = new Label("Application ID:") { Xalign = 0 };
+            id.PackStart(idLabel, false, false, 0);
+            id.PackStart(appId, true, true, 0);
+
+            vbox.PackStart(id, false, false, 0);
+            vbox.ShowAll();
+            return vbox;
+        }
+
+        public override void ApplyChanges()
+        {
+            DiscordRichPresenceOptions.EnableRichPresence.Value = enableButton.Active;
+            DiscordRichPresenceOptions.ShowCurrentFile.Value = showFile.Active;
+            DiscordRichPresenceOptions.ShowCurrentProject.Value = showProject.Active;
+            DiscordRichPresenceOptions.ShowElapsed.Value = showElapsed.Active;
+            DiscordRichPresenceOptions.ApplicationId.Value = appId.Text;
+        }
+    }
+}
