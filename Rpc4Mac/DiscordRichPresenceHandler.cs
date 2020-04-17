bfec6f5e56d8bc92443aeab5ad42d6a229e968b4
reflectronic <john-tur@outlook.com>
2020-04-16 21:29:46 -0400

    Add basic code for detecting workspace

---
+++ Rpc4Mac/DiscordRichPresenceHandler.cs
@@ -0,0 +1,35 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
+
+namespace Rpc4Mac
+{
+    public class DiscordRichPresenceHandler : CommandHandler
+    {
+        protected override void Run()
+        {
+            IdeApp.Workbench.ActiveDocumentChanged += (sender, e) => UpdateRichPresence(e.Document);
+
+            IdeApp.FocusIn += (sender, e) => UpdateRichPresence(IdeApp.Workbench.ActiveDocument);
+
+            IdeApp.Workspace.LastWorkspaceItemClosed += (sender, e) => ShowNoWorkspaceRichPresence();
+
+            ShowNoWorkspaceRichPresence();
+        }
+
+        void ShowNoWorkspaceRichPresence()
+        {
+
+        }
+
+        void UpdateRichPresence(Document currentDocument)
+        {
+            if (currentDocument is null)
+            {
+                // Show generic workspace info
+            }
+
+            MessageService.ShowMessage("Rich Presence Update", $"Your current document is {currentDocument?.Name ?? "None"}");
+        }
+    }
+}
--- Rpc4Mac/Properties/AddinInfo.cs
+++ Rpc4Mac/Properties/AddinInfo.cs
@@ -1,30 +1,4 @@
-﻿//
-// AddinInfo.cs
-//
-// Author:
-//       Mikayla Hutchinson <m.j.hutchinson@gmail.com>
-//
-// Copyright (c) 2017 Microsoft Corp.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-using System;
-using Mono.Addins;
+﻿using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
@@ -35,5 +9,5 @@ using Mono.Addins.Description;
 
 [assembly: AddinName("Rpc4Mac")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Rpc4Mac")]
-[assembly: AddinAuthor("Mikayla Hutchinson")]
+[assembly: AddinDescription("Discord Rich Presense on Visual Studio for Mac")]
+[assembly: AddinAuthor("John Tur")]
